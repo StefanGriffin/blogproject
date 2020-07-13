215d7551c2465322d869733f2ed67467f524649d
Stefan Griffin <griffinstef39@gmail.com>
2020-07-13 12:30:36 -0400

    Added Bootstrap and CSS

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,3 +6,4 @@ urlpatterns = [
     path('about/', views.about, name='blog-about'),
 ]
 
+
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'StefGriffin',
+        'title': 'Blogpost1',
+        'content': 'First post content',
+        'date_posted': 'July 11, 2020',
+
+    },
+    {
+        'author': 'StephanG',
+        'title': 'Blogpost2',
+        'content': 'Second post content',
+        'date_posted': 'July 12, 2020',
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
 
     
